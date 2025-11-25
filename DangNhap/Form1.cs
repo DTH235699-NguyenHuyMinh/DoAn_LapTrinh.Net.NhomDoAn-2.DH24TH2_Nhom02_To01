078e7babbcbee7afe4751a33d20350fa06e1f434
DTH235736_DoanVanPhuc <phuc_dth235736@student.agu.edu.vn>
2025-11-25 15:10:00 +0700

    update

--- DangNhap/Form1.cs
+++ DangNhap/Form1.cs
@@ -136,9 +136,6 @@ namespace DangNhap
             return path;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
